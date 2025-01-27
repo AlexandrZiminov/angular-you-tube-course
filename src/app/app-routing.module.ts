5d953b7318c9830a0fe9404ef9c0efe8e896a9e1
AlexandrZiminov <samazima@mail.ru>
2025-01-27 15:56:54 +0300

    cleared routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {AppComponent} from './app.component';
-import {MyCalculatorComponent} from './calculator/components/my-calculator/my-calculator.component';
 
-const routes: Routes = [
-  {path: '', component: AppComponent}, // Главная страница
-  {path: 'calculator', component: MyCalculatorComponent}, // Страница калькулятора
-  {path: '**', redirectTo: ''}, // Редирект на главную, если маршрут не найден
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)], // Подключаем маршруты
