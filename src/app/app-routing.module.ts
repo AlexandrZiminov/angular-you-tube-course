0a7852bd832fe6c0bc1ce01aab3450d3f2c31cce
AlexandrZiminov <samazima@mail.ru>
2025-01-28 11:47:52 +0300

    added empty-route.component + navigation.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {MyCalculatorComponent} from "./calculator/components/my-calculator/my-calculator.component";
+import {MyEmptyPageComponent} from "./components/empty-route/empty-route.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'calculator',
+    component: MyCalculatorComponent,
+  },
+  {
+    path: '', redirectTo: 'calculator', pathMatch: 'full',
+
+  },
+  {
+    path: '**',
+    component: MyEmptyPageComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)], // Подключаем маршруты
-  exports: [RouterModule], // Экспортируем RouterModule для использования в других модулях
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {MyCalculatorModule} from "./calculator/my-calculator.module";
+import {MyNavigationComponent} from "./components/navigation/navigation.component";
+import {MyEmptyPageComponent} from "./components/empty-route/empty-route.component";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MyCalculatorModule],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, MyNavigationComponent, MyEmptyPageComponent],
+    imports: [BrowserModule, AppRoutingModule, MyCalculatorModule],
+    bootstrap: [AppComponent],
 })
 export class AppModule {
 }
