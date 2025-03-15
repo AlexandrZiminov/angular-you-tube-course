1d14028e7ce08e11f4192c9a45651962165d789b
Alexandr Ziminov <samazima@mail.ru>
2025-03-15 19:05:39 +0300

    animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import {AppRoutingModule} from "./app-routing.module";
 import {MyCalculatorModule} from "./calculator/my-calculator.module";
 import {MyNavigationComponent} from "./components/navigation/navigation.component";
 import {MyEmptyPageComponent} from "./components/empty-route/empty-route.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
     declarations: [AppComponent, MyNavigationComponent, MyEmptyPageComponent],
-    imports: [BrowserModule, AppRoutingModule, MyCalculatorModule],
+    imports: [BrowserAnimationsModule, BrowserModule, AppRoutingModule, MyCalculatorModule],
     bootstrap: [AppComponent],
 })
 export class AppModule {
--- src/app/view-styles/components/view-foo/view-foo.component.ts
+++ src/app/view-styles/components/view-foo/view-foo.component.ts
@@ -1,16 +1,41 @@
 import {ChangeDetectorRef, Component} from '@angular/core';
 import {UserService} from "../../../user.service";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 export interface TodoItemI {
     text: string;
 }
 
+// 1st animation
+const enterTransition = transition(':enter', [
+    style({opacity: 0}),
+    animate('1s ease-in', style({opacity: 1}))
+])
+const leaveTransition = transition(':leave', [
+    style({opacity: 1}),
+    animate('1s ease-out', style({opacity: 0}))
+])
+const fadeIn = trigger('fadeIn', [enterTransition]);
+const fadeOut = trigger('fadeOut', [leaveTransition]);
+
+
+// 2nd animation
+const fadeInOut = trigger('fadeInOut', [
+    state('open', style({opacity: 1})),
+    state('close', style({opacity: 0})),
+    transition('open => close', [animate('1s ease-out')]),
+    transition('close => open', [animate('1s ease-in')])
+]);
+
 @Component({
     selector: 'app-view-foo',
     templateUrl: './view-foo.component.html',
-    styleUrl: './view-foo.component.scss'
+    styleUrl: './view-foo.component.scss',
+    animations: [fadeIn, fadeOut, fadeInOut]
 })
 export class ViewFooComponent {
+
+    public isShow: boolean = false;
 
     public todoArr: TodoItemI[] = [
         {
@@ -39,5 +64,13 @@ export class ViewFooComponent {
 
     public changeStream(): void {
         this._userService.StringSubject$.next('Two')
+    }
+
+    public onAnimationStart(event: any): void {
+        console.log('start', event);
+    }
+
+    public onAnimationDone(event: any): void {
+        console.log('done', event);
     }
 }
